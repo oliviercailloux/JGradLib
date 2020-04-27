5b57edc8dbda658961fcb489aec6a2572b797d63
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-04-27 23:41:08 +0200

    Started destroying Emailer for BetterEmailer

--- src/main/java/io/github/oliviercailloux/email/BetterEmailer.java
+++ src/main/java/io/github/oliviercailloux/email/BetterEmailer.java
@@ -16,4 +16,45 @@ public class BetterEmailer implements AutoCloseable {
 
 	}
 
+	static Session getOutlookImapSession() {
+		final Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imap");
+		props.setProperty("mail.host", "outlook.office365.com");
+		props.setProperty("mail.imap.connectiontimeout", "2000");
+		props.setProperty("mail.imap.timeout", "60*1000");
+		props.setProperty("mail.imap.connectionpooltimeout", "10");
+		props.setProperty("mail.imap.ssl.enable", "true");
+		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
+		final Session session = Session.getInstance(props);
+		return session;
+	}
+
+	static Session getGmailImapSession() {
+		final Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imap");
+		props.setProperty("mail.host", "imap.gmail.com");
+		props.setProperty("mail.imap.connectiontimeout", "2000");
+		props.setProperty("mail.imap.timeout", "60*1000");
+		props.setProperty("mail.imap.connectionpooltimeout", "10");
+		props.setProperty("mail.imap.ssl.enable", "true");
+		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
+		// props.setProperty("mail.debug", "true");
+		final Session session = Session.getInstance(props);
+		return session;
+	}
+
+	static Session getZohoImapSession() {
+		final Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imap");
+		props.setProperty("mail.host", "imap.zoho.eu");
+		props.setProperty("mail.imap.connectiontimeout", "2000");
+		props.setProperty("mail.imap.timeout", "60*1000");
+		props.setProperty("mail.imap.connectionpooltimeout", "10");
+		props.setProperty("mail.imap.ssl.enable", "true");
+		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
+		// props.setProperty("mail.debug", "true");
+		final Session session = Session.getInstance(props);
+		return session;
+	}
+
 }
--- src/main/java/io/github/oliviercailloux/email/Emailer.java
+++ src/main/java/io/github/oliviercailloux/email/Emailer.java
@@ -401,47 +401,6 @@ public class Emailer {
 		}
 	}
 
-	static Session getOutlookImapSession() {
-		final Properties props = new Properties();
-		props.setProperty("mail.store.protocol", "imap");
-		props.setProperty("mail.host", "outlook.office365.com");
-		props.setProperty("mail.imap.connectiontimeout", "2000");
-		props.setProperty("mail.imap.timeout", "60*1000");
-		props.setProperty("mail.imap.connectionpooltimeout", "10");
-		props.setProperty("mail.imap.ssl.enable", "true");
-		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
-		final Session session = Session.getInstance(props);
-		return session;
-	}
-
-	static Session getGmailImapSession() {
-		final Properties props = new Properties();
-		props.setProperty("mail.store.protocol", "imap");
-		props.setProperty("mail.host", "imap.gmail.com");
-		props.setProperty("mail.imap.connectiontimeout", "2000");
-		props.setProperty("mail.imap.timeout", "60*1000");
-		props.setProperty("mail.imap.connectionpooltimeout", "10");
-		props.setProperty("mail.imap.ssl.enable", "true");
-		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
-//		props.setProperty("mail.debug", "true");
-		final Session session = Session.getInstance(props);
-		return session;
-	}
-
-	static Session getZohoImapSession() {
-		final Properties props = new Properties();
-		props.setProperty("mail.store.protocol", "imap");
-		props.setProperty("mail.host", "imap.zoho.eu");
-		props.setProperty("mail.imap.connectiontimeout", "2000");
-		props.setProperty("mail.imap.timeout", "60*1000");
-		props.setProperty("mail.imap.connectionpooltimeout", "10");
-		props.setProperty("mail.imap.ssl.enable", "true");
-		props.setProperty("mail.imap.ssl.checkserveridentity", "true");
-		// props.setProperty("mail.debug", "true");
-		final Session session = Session.getInstance(props);
-		return session;
-	}
-
 	private static Session getSmtpSession() {
 		final Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
