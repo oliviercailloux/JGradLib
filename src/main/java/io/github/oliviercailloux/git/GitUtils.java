594103abd7a92cc15a4aa71acc08afae635983e6
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-12-27 23:56:51 +0100

    Rm old history; started getHistory gitFs

--- src/main/java/io/github/oliviercailloux/git/GitUtils.java
+++ src/main/java/io/github/oliviercailloux/git/GitUtils.java
@@ -1,9 +1,7 @@
 package io.github.oliviercailloux.git;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Verify.verify;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.ZonedDateTime;
@@ -13,11 +11,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.NoHeadException;
-import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -34,6 +27,8 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.Graph;
 import com.google.common.graph.Graphs;
 
+import io.github.oliviercailloux.git.fs.GitFileSystem;
+import io.github.oliviercailloux.git.fs.GitPathRoot;
 import io.github.oliviercailloux.utils.Utils;
 
 public class GitUtils {
@@ -67,46 +62,6 @@ public class GitUtils {
 		return commits.stream().map(RevCommit::getName).collect(ImmutableList.toImmutableList());
 	}
 
-	/**
-	 * @param gitDir the GIT_DIR directory (usually ending with .git)
-	 * @return the graph of commits pointed to by at least one ref in /refs plus
-	 *         HEAD, thus including remotes and local branches and tags, together
-	 *         with their parents.
-	 */
-	public static GitLocalHistory getHistory(File gitDir) throws IOException {
-		final GitLocalHistory history;
-		try (FileRepository repo = new FileRepository(gitDir)) {
-			if (!repo.getObjectDatabase().exists()) {
-				LOGGER.info("No object database at " + gitDir + ", did you forget to use the GIT_DIR?");
-				throw new RepositoryNotFoundException(gitDir);
-			}
-			history = getOldHistory(repo);
-		}
-		return history;
-	}
-
-	public static GitLocalHistory getOldHistory(Repository repository) throws IOException {
-		verify(repository.getObjectDatabase().exists());
-		final GitLocalHistory history;
-		/**
-		 * Log command fails (with org.eclipse.jgit.api.errors.NoHeadException) if “No
-		 * HEAD exists and no explicit starting revision was specified”.
-		 */
-		if (!repository.getRefDatabase().hasRefs()) {
-			return GitLocalHistory.from(ImmutableList.of());
-		}
-		try (Git git = Git.wrap(repository)) {
-			final Iterable<RevCommit> commits = git.log().all().call();
-			history = GitLocalHistory.from(commits);
-			LOGGER.info("Commits: {}.", history.getGraph().nodes());
-		} catch (NoHeadException e) {
-			throw new IllegalStateException(e);
-		} catch (GitAPIException e) {
-			throw new IllegalStateException(e);
-		}
-		return history;
-	}
-
 	public static GitHistory getHistory(Repository repository) throws IOException {
 		/**
 		 * Log command fails (with org.eclipse.jgit.api.errors.NoHeadException) if “No
@@ -132,6 +87,18 @@ public class GitUtils {
 				.collect(ImmutableMap.toImmutableMap(c -> c, c -> getCreationTime(c).toInstant()));
 
 		allCommits.stream().forEach(RevCommit::disposeBody);
+
+		return GitHistory.create(graph, dates);
+	}
+
+	public static GitHistory getHistory(GitFileSystem gitFs) throws IOException {
+		final ImmutableSet<GitPathRoot> commits = gitFs.getCommits();
+
+		final Graph<ObjectId> graph = Graphs.transpose(Utils.asGraph(c -> c.getCommit().getParents(), commits));
+		final ImmutableMap<ObjectId, Instant> dates = commits.stream()
+				.collect(ImmutableMap.toImmutableMap(c -> c, c -> getCreationTime(c).toInstant()));
+
+		commits.stream().forEach(RevCommit::disposeBody);
 
 		return GitHistory.create(graph, dates);
 	}
