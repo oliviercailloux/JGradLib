f1f11ee73ff775d17409bdbbd1f5b47776e12f32
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-01-07 23:46:11 +0100

    Started GitFsGrader using simpler history

--- src/main/java/io/github/oliviercailloux/grade/GitFsGrader.java
+++ src/main/java/io/github/oliviercailloux/grade/GitFsGrader.java
@@ -1,7 +1,9 @@
 package io.github.oliviercailloux.grade;
 
+import io.github.oliviercailloux.git.fs.GitHistorySimple;
+
 public interface GitFsGrader<X extends Exception> {
-	public MarksTree grade(GitFileSystemHistory data) throws X;
+	public MarksTree grade(GitHistorySimple data) throws X;
 
 	public GradeAggregator getAggregator();
 }
