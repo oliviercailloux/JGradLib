3d5b67ddf03dc547d89c6df6af180c4960f32637
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-12-25 22:35:41 +0100

    Mv filtering to own git project

--- src/main/java/io/github/oliviercailloux/git/filter/IGitPathOnFilteredFs.java
+++
@@ -1,7 +0,0 @@
-package io.github.oliviercailloux.git.filter;
-
-import io.github.oliviercailloux.gitjfs.GitPath;
-
-sealed interface IGitPathOnFilteredFs extends GitPath permits IGitPathRootOnFilteredFs, GitPathOnFilteredFs {
-	GitPath delegate();
-}
--- src/main/java/io/github/oliviercailloux/git/filter/IGitPathRootOnFilteredFs.java
+++
@@ -1,9 +0,0 @@
-package io.github.oliviercailloux.git.filter;
-
-import io.github.oliviercailloux.gitjfs.GitPathRoot;
-
-sealed interface IGitPathRootOnFilteredFs extends
-		IGitPathOnFilteredFs permits GitPathRootOnFilteredFs, GitPathRootRefOnFilteredFs, GitPathRootShaOnFilteredFs, GitPathRootShaCachedOnFilteredFs {
-	@Override
-	GitPathRoot delegate();
-}
