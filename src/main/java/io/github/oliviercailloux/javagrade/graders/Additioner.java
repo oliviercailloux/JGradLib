81324a1f5650ed68bbcad00ffa5ee91a01ecf194
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-04-29 17:51:02 +0200

    Junit tests working

--- src/main/java/io/github/oliviercailloux/javagrade/graders/Additioner.java
+++ src/main/java/io/github/oliviercailloux/javagrade/graders/Additioner.java
@@ -1,6 +1,9 @@
 package io.github.oliviercailloux.javagrade.graders;
 
+import static com.google.common.base.Verify.verify;
+
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import io.github.oliviercailloux.exercices.additioner.MyAdditioner;
 import io.github.oliviercailloux.git.github.model.GitHubUsername;
 import io.github.oliviercailloux.git.github.model.RepositoryCoordinates;
@@ -19,6 +22,8 @@ import io.github.oliviercailloux.grade.MavenCodeGrader.WarningsBehavior;
 import io.github.oliviercailloux.grade.format.CsvGrades;
 import io.github.oliviercailloux.jaris.exceptions.TryCatchAll;
 import io.github.oliviercailloux.javagrade.bytecode.Instanciator;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.time.Duration;
@@ -26,6 +31,19 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Set;
+import org.junit.platform.engine.discovery.ClassNameFilter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.LauncherSession;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.junit.platform.launcher.listeners.discovery.LauncherDiscoveryListeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +54,8 @@ public class Additioner implements CodeGrader<RuntimeException> {
   public static final ZonedDateTime DEADLINE_ORIGINAL =
       LocalDateTime.parse("2024-04-10T19:00:00").atZone(ZoneId.of("Europe/Paris"));
   public static final double USER_WEIGHT = 0d;
+
+  public static MyAdditioner studentInstance;
 
   public static void main(String[] args) throws Exception {
     final GitFileSystemWithHistoryFetcher fetcher =
@@ -64,12 +84,51 @@ public class Additioner implements CodeGrader<RuntimeException> {
 
   @Override
   public MarksTree gradeCode(Instanciator instanciator) {
-    final ImmutableMap.Builder<Criterion, MarksTree> builder = ImmutableMap.builder();
-
     final TryCatchAll<MyAdditioner> my = instanciator.tryGetInstance(MyAdditioner.class);
+    // return grade(my);
+
+    MyAdditioner theAdd =
+        my.orThrow(e -> new RuntimeException("Could not instanciate MyAdditioner.", e));
+    studentInstance = theAdd;
+
+    String name = AdditionerTests.class.getCanonicalName();
+    LOGGER.info("Discovering tests in {}.", name);
+    LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+        // .selectors(DiscoverySelectors.selectPackage(Additioner.class.getPackageName())).build();
+        .selectors(DiscoverySelectors.selectClass(AdditionerTests.class)).build();
+    // LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request().build();
+    SummaryGeneratingListener listener = new SummaryGeneratingListener();
+    try (LauncherSession session = LauncherFactory.openSession()) {
+      Launcher launcher = session.getLauncher();
+      launcher.registerTestExecutionListeners(listener);
+      launcher.registerLauncherDiscoveryListeners(LauncherDiscoveryListeners.logging());
+      TestPlan testPlan = launcher.discover(request);
+      verify(testPlan.containsTests(), "No tests found.");
+      verify(testPlan.getRoots().size() == 1);
+      TestIdentifier root = Iterables.getOnlyElement(testPlan.getRoots());
+      LOGGER.info("Root: {}.", root.getDisplayName());
+      verify(root.isContainer());
+      Set<TestIdentifier> children = testPlan.getChildren(root.getUniqueIdObject());
+      verify(!children.isEmpty());
+      verify(children.size() == 1);
+      TestIdentifier child = Iterables.getOnlyElement(children);
+      LOGGER.info("Child: {}.", child.getDisplayName());
+      verify(child.isContainer());
+      launcher.execute(testPlan);
+      TestExecutionSummary summary = listener.getSummary();
+      StringWriter w = new StringWriter();
+      summary.printFailuresTo(new PrintWriter(w));
+      LOGGER.info("Tested: {}", w.toString());
+      
+      return Mark.given(1d - summary.getTotalFailureCount(), summary.getFailures().toString());
+    }
+  }
+
+  private MarksTree grade(TryCatchAll<MyAdditioner> my) {
+    final ImmutableMap.Builder<Criterion, MarksTree> builder = ImmutableMap.builder();
     final TryCatchAll<Integer> added32 = my.andApply(c -> c.add(3, 2));
     builder.put(ADD, added32.map(b -> Mark.binary(b == 5, "", "3 + 2 should equal 5"),
-    c -> Mark.zero("Obtained %s".formatted(c))));
+        c -> Mark.zero("Obtained %s".formatted(c))));
     final TryCatchAll<Integer> addedNeg = my.andApply(c -> c.add(-4, 2));
     builder.put(ADD_NEG, addedNeg.map(b -> Mark.binary(b == -2, "", "-4 + 2 should equal -2"),
         c -> Mark.zero("Obtained %s".formatted(c))));
@@ -84,5 +143,4 @@ public class Additioner implements CodeGrader<RuntimeException> {
     innerBuilder.put(ADD_NEG, 2d);
     return GradeAggregator.staticAggregator(innerBuilder.build(), ImmutableMap.of());
   }
-
 }
--- src/main/java/io/github/oliviercailloux/javagrade/graders/AdditionerTests.java
+++ src/main/java/io/github/oliviercailloux/javagrade/graders/AdditionerTests.java
@@ -1,11 +1,26 @@
 package io.github.oliviercailloux.javagrade.graders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.github.oliviercailloux.exercices.additioner.MyAdditioner;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class AdditionerTests {
-  public MyAdditioner a;
+  public static MyAdditioner a;
 
-  public boolean testAdd() {
-    return a.add(1, 2) == 3;
+  @BeforeAll
+  public static void setUp() {
+    a = Additioner.studentInstance;
+  }
+
+  @Test
+  public void pos() {
+    assertEquals(5, a.add(3, 2));
+  }
+  
+  @Test
+  public void neg() {
+    assertEquals(-2, a.add(-4, 2));
   }
 }
